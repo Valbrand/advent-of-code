8932a66cd31c3d01421eb8ccc0d56e37c86e786a
Daniel Oliveira <daniel.jesus@nubank.com.br>
2020-12-04 09:05:00 -0300

    Day 4 part 1

--- src/advent_of_code_2020/day4.clj
+++ src/advent_of_code_2020/day4.clj
@@ -48,18 +48,8 @@
 (defn day-solution
   []
   (utils/with-lines "resources/day4.txt"
-    (mapv identity (part1-solution lines)) #_
-    (println (str "Part 1: " (part1-solution lines)))))
+    (utils/tap (part1-solution lines))))
 
 (comment
   *e
-  (valid-passport? #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"}
-                   #{"cid"}
-                   {"hcl" "#cfa07d"
-                    "eyr" "2025"
-                    "pid" "166559648"
-                    "iyr" "2011"
-                    "ecl" "brn"
-                    "hgt" "59in"})
-  (parse-lines ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd" "byr:1937 iyr:2017 cid:147 hgt:183cm"])
   (day-solution))
