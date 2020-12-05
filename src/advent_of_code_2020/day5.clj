79d2902518a7c4200ac1d6bc9382317e2d62d26f
Daniel Oliveira <dvalbrand@gmail.com>
2020-12-05 20:24:56 -0300

    Reimplement day 5's find-gaps function with reduce instead of loop

--- src/advent_of_code_2020/day5.clj
+++ src/advent_of_code_2020/day5.clj
@@ -91,6 +91,20 @@
                nil
                nil)))))
 
+(defn find-gaps'
+  [seat-ids]
+  (let [seat-ids (set seat-ids)]
+    (->> (range 1024)
+         (reduce (fn [{:keys [current-gap-start] :as result} n]
+                   (if (contains? seat-ids n)
+                     (assoc result :current-gap-start nil)
+                     (-> result
+                         (update :gaps assoc (or current-gap-start n) n)
+                         (assoc :current-gap-start (or current-gap-start n)))))
+                 {:gaps {}
+                  :current-gap-start nil})
+         :gaps)))
+
 (defn small-gap?
   [[start end]]
   (= start end))
@@ -101,7 +115,7 @@
        (map parse-line)
        (map (partial apply find-position))
        (map (partial apply seat-id))
-       find-gaps
+       find-gaps'
        (filter small-gap?)
        ffirst))
 
