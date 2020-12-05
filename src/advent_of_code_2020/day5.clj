04f347e4029e7567140264fe59ba2dd671bfc5c5
Daniel Oliveira <dvalbrand@gmail.com>
2020-12-05 11:13:10 -0300

    Day 5 part 2

--- src/advent_of_code_2020/day5.clj
+++ src/advent_of_code_2020/day5.clj
@@ -59,12 +59,58 @@
        (map (partial apply seat-id))
        (apply max)))
 
+(defn find-gaps
+  [seat-ids]
+  (let [seat-ids (set seat-ids)
+        new-gap  vector]
+    (loop [[n & numbers-to-check] (range 1024)
+           gaps #{}
+           gap-started-at nil
+           last-number-in-gap nil]
+      (cond
+        (nil? n)
+        (if (nil? gap-started-at)
+          gaps
+          (conj gaps (new-gap gap-started-at last-number-in-gap)))
+
+        (not (contains? seat-ids n))
+        (recur numbers-to-check
+               gaps
+               (or gap-started-at n)
+               n)
+
+        (nil? gap-started-at)
+        (recur numbers-to-check
+               gaps
+               nil
+               nil)
+
+        :else
+        (recur numbers-to-check
+               (conj gaps (new-gap gap-started-at last-number-in-gap))
+               nil
+               nil)))))
+
+(defn small-gap?
+  [[start end]]
+  (= start end))
+
+(defn part2-solution
+  [lines]
+  (->> lines
+       (map parse-line)
+       (map (partial apply find-position))
+       (map (partial apply seat-id))
+       find-gaps
+       (filter small-gap?)
+       ffirst))
+
 (defn day-solution
   []
   (utils/with-lines "resources/day5.txt"
     (fn [lines]
       (utils/tap (part1-solution lines))
-      #_(utils/tap (part2-solution lines))))
+      (utils/tap (part2-solution lines))))
   nil)
 
 (comment
