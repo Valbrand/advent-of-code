847d72ffa5af4cf7dc5ed51e81260c9a5a4cbfeb
Daniel Oliveira <dvalbrand@gmail.com>
2020-12-06 10:49:37 -0300

    Day 6 part 2

--- src/advent_of_code_2020/day6.clj
+++ src/advent_of_code_2020/day6.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code-2020.day6
-  (:require [advent-of-code-2020.utils :as utils]))
+  (:require [advent-of-code-2020.utils :as utils]
+            [clojure.set :as set]))
 
 (defn group-yes-answers
   [group-answers]
@@ -14,11 +15,26 @@
        (map group-yes-answers)
        (reduce + 0)))
 
+(defn group-all-yes-answers
+  [group-answers]
+  (->> group-answers
+       (map set)
+       (apply set/intersection)
+       count))
+
+(defn part2-solution
+  [lines]
+  (->> lines
+       utils/split-by-empty-lines
+       (map group-all-yes-answers)
+       (reduce + 0)))
+
 (defn day-solution
   []
   (utils/with-lines "resources/day6.txt"
     (fn [lines]
-      (utils/tap (part1-solution lines))))
+      (utils/tap (part1-solution lines))
+      (utils/tap (part2-solution lines))))
   nil)
 
 (comment
